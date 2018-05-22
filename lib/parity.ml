4a9b1fc1c05cc14f286e96af97b830c64b3e5fff
Ilias <ilias.gar@gmail.com>
2018-05-22 16:08:13 +0200

    added add_reserved_peer

--- lib/parity.ml
+++ lib/parity.ml
@@ -5,3 +5,8 @@ module Http_client = Nethttp_client
 let new_account_from_phrase uri (id : Identity.t) =
   let args = Yojson.Basic.(`List [ `String id.login; `String id.pwd ]) in
   Rpc.call uri "parity_newAccountFromPhrase" args
+
+let add_reserved_peer uri enode =
+  let open Yojson.Basic in
+  let params = `List [`String enode] in
+  Rpc.call uri "parity_addReservedPeer" params
