1bd152e5bc109d0bc83d1dedc5c7359fdc7a9e9e
Ilias <ilias.gar@gmail.com>
2018-06-05 12:02:55 +0200

    added example

---
+++ examples/deploy.ml
@@ -0,0 +1,60 @@
+open Mlparity
+open Ssh_client
+
+let genesis =
+  GethInit.Genesis.({
+      config = {
+        chain_id        = 8888; (* never put 0!!! *)
+        homestead_block = 0;
+        eip155_block    = 0;
+        eip158_block    = 0
+      };
+
+      alloc = [];
+
+      coinbase    = "0x0000000000000000000000000000000000000000";
+      difficulty  = 1;
+      extra_data  = "";
+      gas_limit   = 0x80000000;
+      nonce       = 0x107;
+      mix_hash    = "0x0000000000000000000000000000000000000000000000000000000000000000";
+      parent_hash = "0x0000000000000000000000000000000000000000000000000000000000000000";
+      timestamp   = 0;
+  })
+
+let conf =
+  let open GethInit in
+  {
+    genesis_block  = genesis;
+    network_id     = 8798798;
+    root_directory = "priveth";
+    data_subdir    = "data";
+    source_subdir  = "source"
+  }
+
+let pepito_on_laptop () =
+  { GethInit.ip_address = "127.0.0.1";
+    ssh_port = 22;
+    login = "pepito";
+    password = Easy.input_password ~host:"127.0.0.1" ~username:"pepito"
+  }
+
+let pepito_on_server () =
+  { GethInit.ip_address = "129.x.y.z";
+    ssh_port = 22;
+    login = "pepito";
+    password = Easy.input_password ~host:"just for display purposes" ~username:"pepito"
+  }
+
+let conchita_on_osx () =
+  { GethInit.ip_address = "82.x.y.z";
+    ssh_port = 22;
+    login = "conchita";
+    password = Easy.input_password ~host:"conchitas_apple_II" ~username:"conchita"
+  }
+
+
+let network = [ pepito_on_laptop (); pepito_on_server; conchita_on_osx () ]
+
+let _ =
+  GethInit.deploy conf network
